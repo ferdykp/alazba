4394e86d5f2edbe3b4aea6b684a8b9c29c292c05
itdev22 <90917112+itdev22@users.noreply.github.com>
2023-06-15 10:12:46 +0700

    Update footer.php

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -6,23 +6,26 @@
 	<!-- ./Sweet Alert 2 -->
 
 	<!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="<?= base_url('assets/vendor/jquery/jquery-3.5.1.min.js'); ?>"></script>
-    <script src="<?= base_url('assets/vendor/jquery/popper.min.js'); ?>"></script>
-    <script src="<?= base_url('assets/vendor/bootstrap/js/bootstrap.min.js'); ?>"></script>
-    <script src="<?= base_url('assets/vendor/sweetalert2/sweetalert2.all.min.js'); ?>"></script>
-    <!-- Plugins -->
-    <script src="<?= base_url('assets/vendor/datatables/js/jquery.dataTables.min.js'); ?>"></script>
-    <script src="<?= base_url('assets/vendor/datatables/js/dataTables.bootstrap4.min.js'); ?>"></script>
-    <script src="<?= base_url('assets/vendor/fontawesome/js/all.min.js'); ?>"></script>
-    <script src="<?= base_url('assets/vendor/jquery-easing/jquery.easing.min.js'); ?>"></script>
-    <script src="<?= base_url('assets/vendor/sweatalert2/sweetalert2.all.min.js'); ?>"></script>
-    <script src="<?= base_url('assets/vendor/jquery-mcustomscrollbar/jquery.mCustomScrollbar.concat.min.js'); ?>"></script>
-    <!-- Configuration Plugins -->
-    <script src="<?= base_url('assets/js/config-datatables.js'); ?>"></script>
-    <script src="<?= base_url('assets/js/config-popper.js'); ?>"></script>
-    <script src="<?= base_url('assets/js/config-sweatalert2.js'); ?>"></script>
-    <!-- MyJS -->
-    <script src="<?= base_url('assets/js/alazba.js'); ?>"></script>
-  </body>
-</html>
+	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	<script src="<?= base_url('assets/vendor/jquery/jquery-3.5.1.min.js'); ?>"></script>
+	<script src="<?= base_url('assets/vendor/jquery/popper.min.js'); ?>"></script>
+	<script src="<?= base_url('assets/vendor/bootstrap/js/bootstrap.min.js'); ?>"></script>
+	<script src="<?= base_url('assets/vendor/sweetalert2/sweetalert2.all.min.js'); ?>"></script>
+	<!-- Plugins -->
+	<script src="<?= base_url('assets/vendor/datatables/js/jquery.dataTables.min.js'); ?>"></script>
+	<script src="<?= base_url('assets/vendor/datatables/js/dataTables.bootstrap4.min.js'); ?>"></script>
+	<script src="<?= base_url('assets/vendor/fontawesome/js/all.min.js'); ?>"></script>
+	<script src="<?= base_url('assets/vendor/jquery-easing/jquery.easing.min.js'); ?>"></script>
+	<script src="<?= base_url('assets/vendor/sweatalert2/sweetalert2.all.min.js'); ?>"></script>
+	<script src="<?= base_url('assets/vendor/jquery-mcustomscrollbar/jquery.mCustomScrollbar.concat.min.js'); ?>"></script>
+	<!-- Configuration Plugins -->
+	<script src="<?= base_url('assets/js/config-datatables.js'); ?>"></script>
+	<script src="<?= base_url('assets/js/config-popper.js'); ?>"></script>
+	<script src="<?= base_url('assets/js/config-sweatalert2.js'); ?>"></script>
+	<!-- MyJS -->
+	<script src="<?= base_url('assets/js/alazba.js'); ?>"></script>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+
+	</body>
+
+	</html>
