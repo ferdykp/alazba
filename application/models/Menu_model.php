8c3efba233c4f7355367ba30418ca2c154a5b9aa
itdev22 <90917112+itdev22@users.noreply.github.com>
2023-06-15 09:35:51 +0700

    Update Menu_model.php

--- application/models/Menu_model.php
+++ application/models/Menu_model.php
@@ -39,7 +39,7 @@ class Menu_model extends CI_Model
 		$this->checkJabatan('menambahkan menu');
 
 		$config['upload_path']         = 'images/';  // folder upload 
-		$config['allowed_types']        = 'gif|jpg|png'; // jenis file
+		$config['allowed_types']        = 'gif|jpg|png|jpeg'; // jenis file
 		$config['max_size']             = 3000;
 		$config['max_width']            = 1024;
 		$config['max_height']           = 768;
