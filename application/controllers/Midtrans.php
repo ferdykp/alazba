0ddaa9e5541d992aadc760d8ab36dea6c32dba30
itdev22 <90917112+itdev22@users.noreply.github.com>
2023-06-19 20:56:05 +0700

    Update Midtrans.php

--- application/controllers/Midtrans.php
+++ application/controllers/Midtrans.php
@@ -63,7 +63,7 @@ class Midtrans extends CI_Controller
 		$result = json_decode($json_result);
 
 
-		$this->pm->update($result['order_id']);
+		$this->pm->updateMidtrans($result['order_id']);
 		return $this->output
 			->set_content_type('application/json')
 			->set_status_header(200)
