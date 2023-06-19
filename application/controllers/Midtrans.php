b0fcf3d013e66a1d85231c136faf7c0c929e9e3e
itdev22 <90917112+itdev22@users.noreply.github.com>
2023-06-19 09:07:47 +0700

    Update Midtrans.php

---
+++ application/controllers/Midtrans.php
@@ -0,0 +1,45 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Midtrans extends CI_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('Log_model', 'lm');
+		$this->load->model('Main_model', 'mm');
+	}
+
+	public function index()
+	{
+		return $this->output
+			->set_content_type('application/json')
+			->set_status_header(200)
+			->set_output(json_encode([
+				'text' => 'Error 200',
+				'type' => 'danger'
+			]));
+	}
+
+	public static function request()
+	{
+		return $this->output
+			->set_content_type('application/json')
+			->set_status_header(200)
+			->set_output(json_encode([
+				'text' => 'Error 200',
+				'type' => 'danger'
+			]));
+	}
+
+	public function callback()
+	{
+		return $this->output
+			->set_content_type('application/json')
+			->set_status_header(200)
+			->set_output(json_encode([
+				'text' => 'Error 200',
+				'type' => 'danger'
+			]));
+	}
+}
