3ae95f6e95cf16a25c9ba1dd964b3f6e406853b5
itdev22 <90917112+itdev22@users.noreply.github.com>
2023-06-19 20:04:31 +0700

    Update Midtrans.php

--- application/controllers/Midtrans.php
+++ application/controllers/Midtrans.php
@@ -21,15 +21,39 @@ class Midtrans extends CI_Controller
 			]));
 	}
 
-	public static function request()
+	public static function request($kode_pembayaran, $nominal)
 	{
-		return $this->output
-			->set_content_type('application/json')
-			->set_status_header(200)
-			->set_output(json_encode([
-				'text' => 'Error 200',
-				'type' => 'danger'
-			]));
+		$curl = curl_init();
+
+		$params = array(
+			'transaction_details' => array(
+				'order_id' => $kode_pembayaran,
+				'gross_amount' => $nominal,
+			)
+		);
+
+
+		curl_setopt_array($curl, array(
+			CURLOPT_URL => 'https://app.sandbox.midtrans.com/snap/v1/transactions',
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_ENCODING => '',
+			CURLOPT_MAXREDIRS => 10,
+			CURLOPT_TIMEOUT => 0,
+			CURLOPT_FOLLOWLOCATION => true,
+			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+			CURLOPT_CUSTOMREQUEST => 'POST',
+			CURLOPT_POSTFIELDS => json_encode($params),
+			CURLOPT_HTTPHEADER => array(
+				'accept: application/json',
+				'authorization: Basic U0ItTWlkLXNlcnZlci1GX2hiX045S2pWMS1IWlh6YktCZ01uLWQ6aXJiYUtlcmVuMjM=',
+				'content-type: application/json'
+			),
+		));
+
+		$response = curl_exec($curl);
+		curl_close($curl);
+
+		return $response;
 	}
 
 	public function callback()
